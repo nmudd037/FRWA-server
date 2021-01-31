8ac99354d4af27a8fa5d09f6e49f0cce34964b85
nmudd037 <nithishreddymuddasani.7@gmail.com>
2021-01-30 20:22:11 -0500

    Final Changes

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
+const compression = require('compression');
 const knex = require('knex');
 const { v4: uuidv4 } = require('uuid');
 
@@ -13,8 +18,35 @@ const image = require('./controllers/image');
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+// Implement CORS
+app.use(cors()); //Sets Access-Control-Allow-Origin: *
+app.options('*', cors());
+
+app.use(helmet());
+
+//We use Morgan Middleware based on whether we are in development environment or production environment
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//Implementing Rate Limiting - Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/', limiter);
+
+//Body Parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); //Middleware
+app.use(express.urlencoded({ extended: true, limit: '10kb' })); //URL encoded data parser
+
+//Data Sanitization against XSS Attacks
+app.use(xss());
+
+//Compression
+app.use(compression());
 
 const db = knex(knexConfig[process.env.NODE_ENV]);
 
@@ -44,6 +76,12 @@ app.put('/image', (req, res) => {
 
 app.post('/imageurl', (req, res) => {
   image.handleApiCall(req, res);
+});
+
+//Handling Unhandled Routes
+app.all('*', (req, res, next) => {
+  res.status(404).json(`Can't find ${req.originalUrl} on this server`);
+  next();
 });
 
 //4) Start Server
